26425fa5e247d0e0fd1d51726a28b3750587fa0f
Ramash3828 <ramash3828@gmail.com>
2022-06-10 23:34:28 +0600

    about page done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,8 +3,21 @@ import "./About.css";
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className="py-5">
+            <div className="contianer">
+                <h2 className="text-uppercase fw-bold title">About Me</h2>
+                <div className="line mb-5"></div>
+                <p className="text-white w-50 mx-auto">
+                    Building a career with any national or international
+                    organization so that I can use my learning and potentiality
+                    properly for the development of the organization as well as
+                    myself. As a web application developer adept at building web
+                    applications from front end to back end. Passionate, new
+                    technologies bring ideas to life and work with dedicated
+                    teams to build efficient and robust applications suited to
+                    the user’s needs.
+                </p>
+            </div>
         </div>
     );
 };
