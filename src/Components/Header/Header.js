a2fc2282976c0030effcc9e29fee5ec45a541629
Ramash3828 <ramash3828@gmail.com>
2022-06-11 00:48:50 +0600

    click the menu item and collapse responsive menu area

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,28 +2,29 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
-    const [isClick, setIsClick] = useState(false);
+    const [clicked, setClicked] = useState(false);
     return (
         <nav className="navbar navbar-expand-lg navbar-light my-nav">
             <div className="container">
                 <button
-                    className="navbar-toggler "
+                    className={`navbar-toggler ${clicked ? "collapsed" : ""}`}
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
+                    aria-expanded={clicked}
                     aria-label="Toggle navigation"
                 >
                     <i className="fa-solid fa-bars"></i>
                 </button>
                 <div
-                    className="collapse navbar-collapse"
+                    className={`collapse navbar-collapse ${clicked && ""}`}
                     id="navbarSupportedContent"
                 >
                     <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <NavLink
+                                onClick={() => setClicked(!clicked)}
                                 to="/"
                                 className="nav-link "
                                 aria-current="page"
@@ -33,6 +34,7 @@ const Header = () => {
                         </li>
                         <li className="nav-item">
                             <NavLink
+                                onClick={() => setClicked(!clicked)}
                                 to="about"
                                 className="nav-link "
                                 aria-current="page"
@@ -42,6 +44,7 @@ const Header = () => {
                         </li>
                         <li className="nav-item">
                             <NavLink
+                                onClick={() => setClicked(!clicked)}
                                 to="resume"
                                 className="nav-link "
                                 aria-current="page"
@@ -51,6 +54,7 @@ const Header = () => {
                         </li>
                         <li className="nav-item">
                             <NavLink
+                                onClick={() => setClicked(!clicked)}
                                 to="contact"
                                 className="nav-link "
                                 aria-current="page"
